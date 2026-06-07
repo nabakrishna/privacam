ac480262dd233baa77bb11c4d94c8e3250a98466
nabakrishna <nabakrishna0111@gmail.com>
2026-06-07 12:19:56 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 from ultralytics import YOLO
+model = YOLO("yolov8s-seg.pt") 
+results = model.predict(source="test_debit_card.jpg", conf=0.25)
 
-# This automatically downloads the weights for you
-model = YOLO("yolo26s-seg.pt")
+for result in results:
+    for box in result.boxes:
+        cls_id = int(box.cls[0].item())
+        confidence = box.conf[0].item()
+        #map model ids etc
+        print(f"Detected Class ID: {cls_id} with {confidence:.2f} confidence")
